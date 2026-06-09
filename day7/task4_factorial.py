ecb430e61eeb530439620a3891838a7fe75422f5
shanavora10 <shanavora10@gmail.com>
2026-06-09 16:31:26 +0530

    day7 completed

---
+++ day7/task4_factorial.py
@@ -0,0 +1,7 @@
+n =int(input("Enter a number:"))
+result =1
+
+for i in range(1, n+1):
+    result =result * i
+
+print("Factorial=",result)
---
+++ day7/task5_pattern.py
@@ -0,0 +1,14 @@
+
+print("--- Method 1: String Multiplication ---")
+for i in range(1,6):
+    print("*" * i)
+
+print()
+
+print("---Method 2: Nested loop---")
+for i in range(1,6):
+    for j in range(i):
+        print("*",end="")
+        print()
+
+        
